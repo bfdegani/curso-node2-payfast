83bc04898cf5a5b9d4d790600b02cc9dafd4d43a
bfdegani <bruno.degani@gmail.com>
2017-09-09 12:36:35 -0300

    versao inicial

---
+++ config/express.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var consign = require('consign');
+
+module.exports = function() {
+    var app = express();
+
+    consign({cwd:'app'}).include('routes').into(app);
+
+    return app;
+}
