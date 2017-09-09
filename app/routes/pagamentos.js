0d39072ac6fc67caa9337605752f0f86cbff8797
bfdegani <bruno.degani@gmail.com>
2017-09-09 13:11:44 -0300

    lendo entrada json

--- app/routes/pagamentos.js
+++ app/routes/pagamentos.js
@@ -1,5 +1,15 @@
 module.exports = function(app) {
-   app.get("/pagamentos",function(req, res) {
+   app.get('/pagamentos',function(req, res) {
         res.send('ok');
+    });
+
+    app.post('/pagamentos/pagamento', function(req,res){
+      var pagamento = req.body;
+      console.log(pagamento);
+
+      pagamento.status = 'CRIADO';
+      pagamento.data_pagamento = new Date();
+
+      res.send(pagamento);
     });
 }
--- config/express.js
+++ config/express.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
     var app = express();
 
+    app.use(bodyParser.json());
+    
     consign({cwd:'app'}).include('routes').into(app);
 
     return app;
