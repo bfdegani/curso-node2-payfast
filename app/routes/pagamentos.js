ae7a848df3ea40e3ff140e8f938514a51d835a11
bfdegani <bruno.degani@gmail.com>
2017-10-23 00:42:35 -0200

    recuperando implementacao perdida e pagamentos.js

--- app/routes/pagamentos.js
+++ app/routes/pagamentos.js
@@ -1,15 +1,185 @@
 module.exports = function(app) {
-   app.get('/pagamentos',function(req, res) {
-        res.send('ok');
+
+function HATEOAS(pagamento_id){
+  //enriquecendo o retorno com as operações possíveis na api após a criação do pagamento
+  //Esse tipo de enriquecimento é conhecido como HATEOAS (Hypermedia as the Engine of Application State)
+  var links = [
+    {
+      href: "/pagamentos/pagamento/" + pagamento_id,
+      rel: "confirmar",
+      method: "PUT"
+    },
+    {
+      href: "/pagamentos/pagamento/" + pagamento_id,
+      rel: "cancelar",
+      method: "DELETE"
+    },
+    {
+      href: "/pagamentos/pagamento/" + pagamento_id,
+      rel: "buscar",
+      method: "GET"
+    }
+  ];
+  return links;
+}
+
+  //lista todos os pagamentos
+  app.get('/pagamentos',function(req, res) {
+
+    var connection = app.database.connectionFactory();
+    var pagamentoDAO = new app.database.PagamentoDAO(connection);
+
+    pagamentoDAO.lista(function(err,result){
+      if(err){
+        console.log("erro ao listar pagamentos:" + err);
+        res.status(500).json(err);
+      }
+      else
+      {
+        console.log('pagamentos:');
+        console.log(result);
+        if(result == '')
+          res.status(404).json(result);
+        else
+          res.status(200).json(result);
+      }
     });
+  });
 
-    app.post('/pagamentos/pagamento', function(req,res){
-      var pagamento = req.body;
-      console.log(pagamento);
+  //busca um pagamento especifico
+  app.get('/pagamentos/pagamento/:id',function(req, res) {
 
-      pagamento.status = 'CRIADO';
-      pagamento.data_pagamento = new Date();
+    var id = req.params.id;
 
-      res.send(pagamento);
+    var connection = app.database.connectionFactory();
+    var pagamentoDAO = new app.database.PagamentoDAO(connection);
+    pagamentoDAO.buscaPorId(id, function(err,result){
+      if(err){
+        console.log("erro ao buscar pagamento " + id + ": "+ err);
+        res.status(500).json(err);
+      }
+      else
+      {
+        console.log('pagamento:');
+        console.log(result);
+        if(result == '')
+          res.status(404).json(result);
+        else
+          res.status(200).json(result);
+      }
     });
+  });
+
+  //cria um novo pagamento
+  app.post('/pagamentos/pagamento', function(req,res){
+
+      // valida json de entrada
+      req.assert("pagamento.forma_pagamento", "Forma de pagamento obrigatoria").notEmpty();
+      req.assert("pagamento.valor","Valor obrigatorio e decimal").notEmpty().isFloat();
+      req.assert("pagamento.moeda", "Moeda obrigatoria").notEmpty();
+
+      req.getValidationResult().then(function(err){
+        if(!err.isEmpty()){
+          console.log("arquivo de entrada apresenta erros: ");
+          console.log(err.array());
+          res.status(400).json(err.array());
+          return;
+        }
+
+        var pagamento = req.body.pagamento;
+        pagamento.status = 'CRIADO';
+
+        var infoResponse = {};
+        //console.log(pagamento);
+
+        //se for pagamento via cartao, autoriza
+        if(pagamento.forma_pagamento == 'cartao'){
+          var cartao = req.body.cartao;
+          infoResponse.dados_cartao = cartao;
+
+          var clienteCartoes = new app.servicos.ClienteCartoes();
+          clienteCartoes.autoriza(cartao, function(erros, request, response, retorno){
+            if(erros){
+              console.log(erros);
+              res.status(400).send(erros);
+              return;
+            }
+          });
+          pagamento.status = 'AUTORIZADO';
+        }
+
+        //cria registro do pagamento
+        var connection = app.database.connectionFactory();
+        var pagamentoDAO = new app.database.PagamentoDAO(connection);
+
+        pagamentoDAO.salva(pagamento, function(err, result){
+          if(err){
+            console.log("erro ao criar pagamento:" + err);
+            res.status(500).json(err);
+            return;
+          }
+
+          console.log('pagamento criado');
+          //console.log(pagamento);
+          pagamento.id = result.insertId;
+
+          res.location('/pagamentos/pagamento/' + pagamento.id);
+          infoResponse.dados_pagamento = pagamento;
+          infoResponse.links = HATEOAS(pagamento.id);
+
+          res.status(201).json(infoResponse); // http status 201: created
+        });
+      });
+
+      //altera status do pagamento para CONFIRMADO
+      app.put('/pagamentos/pagamento/:id', function(req, res){
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = "CONFIRMADO";
+
+        var connection = app.database.connectionFactory();
+        var pagamentoDAO = new app.database.PagamentoDAO(connection);
+
+        pagamentoDAO.atualiza(pagamento, function(err){
+          if(err){
+            console.log("erro ao confirmar pagamento:" + err);
+            res.status(500).json(err);
+          }
+          else
+          {
+            console.log('pagamento confirmado');
+            console.log(pagamento);
+            res.status(200).json(pagamento);
+          }
+      });
+
+    });
+  });
+
+  // cancelamento do pagamento
+  app.delete('/pagamentos/pagamento/:id', function(req, res){
+    var pagamento = {}
+    var id = req.params.id;
+
+    pagamento.id = id;
+    pagamento.status = "CANCELADO";
+
+    var connection = app.database.connectionFactory();
+    var pagamentoDAO = new app.database.PagamentoDAO(connection);
+
+    pagamentoDAO.atualiza(pagamento, function(err){
+      if(err){
+        console.log("erro ao cancelar pagamento:" + err);
+        res.status(500).json(err);
+      }
+      else
+      {
+        console.log('pagamento cancelado');
+        console.log(pagamento);
+        res.status(200).json(pagamento);
+      }
+    });
+  });
 }
